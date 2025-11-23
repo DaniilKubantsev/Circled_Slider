d4121697906773c622a183a08d66357e589d51ca
Daniil Kubantsev <daniil.kubantsev@gmail.com>
2025-11-23 22:42:09 +0300

    Thumbs, drag gestures logic, side functions

--- app/src/main/java/com/example/circledslider/presentation/components/circled_slider/CircledRangeSlider.kt
+++ app/src/main/java/com/example/circledslider/presentation/components/circled_slider/CircledRangeSlider.kt
@@ -1,8 +1,10 @@
 package com.example.circledslider.presentation.components.circled_slider
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,14 +12,18 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material3.RangeSlider
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -31,21 +37,37 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.center
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.toOffset
+import com.example.circledslider.ui.theme.DarkOliveGreen
+import com.example.circledslider.ui.theme.Olivine
 import com.example.circledslider.ui.theme.Parchment
+import com.example.circledslider.ui.theme.Umber
+import kotlin.math.abs
+import kotlin.math.atan2
+import kotlin.math.cos
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.sin
 
 
 @Composable
 fun CircledRangeSlider(
-    onStartValueChange: (Float) -> Unit,
-    onFinishValueChange: (Float) -> Unit,
+    startAngle: Float,
+    finishAngle: Float,
+    onStartAngleChange: (Float) -> Unit,
+    onFinishAngleChange: (Float) -> Unit,
+    minDegree: Float = 0f,
+    maxDegree: Float = 360f,
+    minimalGap: Float = 10f,
     enabled: Boolean = true,
     color: CircledSliderColors,
     thumbRadius: Float,
@@ -54,46 +76,196 @@ fun CircledRangeSlider(
     paddingValues: PaddingValues,
     modifier: Modifier
 ) {
-    val activeTrackColor: Color = Color.Red
-    var size by remember { mutableStateOf(IntSize.Zero) }
-    var radius by remember { mutableFloatStateOf(100f) }
+    val density = LocalDensity.current
 
-    var startAngle by remember { mutableFloatStateOf(30f) }
-    var finishAngle by remember { mutableFloatStateOf(120f) }
+    // Component diameter in px
+    var diameter by remember { mutableFloatStateOf(0f) }
+
+
+    var startValue by remember { mutableFloatStateOf(startAngle) }
+    var finishValue by remember { mutableFloatStateOf(finishAngle) }
+
+    var isHeightBigger by remember { mutableStateOf(false) }
+
+    val mx = with(density) { (max(activeTrackThickness, inactiveTrackThickness)/2).toDp() }
+    val padding = PaddingValues(mx)
+
+    val thumbSize = with(density) {(2 * thumbRadius).toDp()}
     
-    val padding = PaddingValues((max(activeTrackThickness, inactiveTrackThickness)/4).dp)
-
-    radius = min(size.height, size.width).toFloat()
+    var thumbCol by remember { mutableStateOf(Color.Transparent) }
+    thumbCol = color.thumbColor()
 
 
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
-            .aspectRatio(1f)
+
             .rotate(-90f)
             .onGloballyPositioned {
-                size = it.size
+                isHeightBigger = it.size.width > it.size.height
             }
             .then(modifier)
-            .padding(paddingValues=padding)
+            .aspectRatio(1f)
+            .padding(paddingValues = padding)
     ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            diameter = this.size.minDimension
+        }
+
         InactiveTrack(
             color = color.inactiveTrackColor(),
             thickness = inactiveTrackThickness,
+            isHeightBigger = isHeightBigger
         )
 
         ActiveTrack(
-            startAngle = startAngle,
-            finishAngle = finishAngle,
+            startAngle = startValue,
+            finishAngle = finishValue,
             color = color.activeTrackColor(),
             thickness = activeTrackThickness,
+            isHeightBigger = isHeightBigger
         )
 
+
+        // START THUMB
+        Canvas(
+            modifier = Modifier
+                .size(thumbSize)
+                .offset {
+                    IntOffset(
+                        (diameter / 2 * cos(Math.toRadians(startValue.toDouble()))).toInt(),
+                        (diameter / 2 * sin(Math.toRadians(startValue.toDouble()))).toInt()
+                    )
+                }
+                .pointerInput(enabled) {
+                    detectDragGestures(
+                        onDrag = { _, dragAmount ->
+                            
+                            var currentPosition = getCurrentPosition(startValue, diameter / 2)
+                            currentPosition += dragAmount
+                            val newAngle = calculateNewAngle(currentPosition)
+                            Log.w("START_THUMB__ON_DRUG", "cond: ${newAngle in minDegree..(finishValue - minimalGap)} | newAngle: ${newAngle}")
+                            if (enabled && newAngle in minDegree..(finishValue - minimalGap)) {
+                                startValue = newAngle
+                            }
+
+                            onFinishAngleChange(startValue)
+                        }
+                    )
+                }
+
+        ) {
+            drawCircle(
+                color = thumbCol,
+                radius = thumbRadius,
+                center = center,
+            )
+        }
+
+
+        // FINISH THUMB
+        Canvas(
+            modifier = Modifier
+                .size(thumbSize)
+                .offset {
+                    IntOffset(
+                        (diameter / 2 * cos(Math.toRadians(finishValue.toDouble()))).toInt(),
+                        (diameter / 2 * sin(Math.toRadians(finishValue.toDouble()))).toInt()
+                    )
+                }
+                .pointerInput(enabled) {
+                    detectDragGestures(
+                        onDrag = { _, dragAmount ->
+
+                            var currentPosition = getCurrentPosition(finishValue, diameter / 2)
+                            currentPosition += dragAmount
+                            val newAngle = calculateNewAngle(currentPosition)
+
+                            if (enabled && newAngle in (startValue + minimalGap)..maxDegree) {
+                                finishValue = newAngle
+                            }
+
+                            onFinishAngleChange(finishValue)
+                        }
+                    )
+                }
+
+        ) {
+            drawCircle(
+                color = thumbCol,
+                radius = thumbRadius,
+                center = center,
+            )
+        }
 
 
     }
 
 }
+
+
+private fun getCurrentPosition(
+    angle: Float,
+    radius: Float
+): Offset {
+    return Offset(
+        x = radius * cos(Math.toRadians(angle.toDouble())).toFloat(),
+        y = radius * sin(Math.toRadians(angle.toDouble())).toFloat()
+    )
+}
+
+private fun calculateNewAngle(
+    currentPosition: Offset,
+    center: Offset = Offset.Zero,
+): Float {
+    val (dx, dy) = currentPosition - center
+    val radians = atan2(dy, dx).toDouble()
+    var degrees = Math.toDegrees(radians).toFloat()
+    if (degrees < 0) degrees += 360f
+    return degrees
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun CircledRangeSliderPreview(){
+
+    val thumbRadius = 50f
+    val activeTrackThickness = 130f
+    val inactiveTrackThickness = 100f
+
+    var start by remember { mutableFloatStateOf(30f) }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Parchment)
+    ) {
+        CircledRangeSlider(
+            startAngle = start,
+            finishAngle = 100f,
+            onStartAngleChange = { start = it},
+            onFinishAngleChange = {},
+            color = CircledRangeSliderDefaults.circledSliderColors(
+                inactiveTrackColor = DarkOliveGreen,
+                activeTrackColor = Umber,
+                thumbColor = Parchment
+            ),
+            paddingValues = PaddingValues(),
+            thumbRadius = thumbRadius,
+            activeTrackThickness = activeTrackThickness,
+            inactiveTrackThickness = inactiveTrackThickness,
+            modifier = Modifier
+
+        )
+    }
+}
+
 
 
 @Composable
@@ -102,10 +274,12 @@ fun ActiveTrack(
     finishAngle: Float,
     color: Color,
     thickness: Float,
+    isHeightBigger: Boolean,
 ) {
     Canvas(
         modifier = Modifier
-            .aspectRatio(1f)
+            .aspectRatio(1f, isHeightBigger)
+            //.fillMaxSize()
     ) {
         drawArc(
             color = color,
@@ -119,8 +293,7 @@ fun ActiveTrack(
             style = Stroke(
                 width = thickness,
                 cap = StrokeCap.Round
-            ),
-            alpha = 0.7f
+            )
         )
     }
 }
@@ -130,10 +303,12 @@ fun ActiveTrack(
 fun InactiveTrack(
     color: Color,
     thickness: Float,
+    isHeightBigger: Boolean,
 ) {
     Canvas(
         modifier = Modifier
-            .aspectRatio(1f)
+            .aspectRatio(1f, isHeightBigger)
+
     ) {
         drawCircle(
             color = color,
@@ -145,38 +320,6 @@ fun InactiveTrack(
 }
 
 
-@Preview(showBackground = true)
-@Composable
-fun CircledRangeSliderPreview(){
-
-    val thumbRadius = 10f
-    val activeTrackThickness = 100f
-    val inactiveTrackThickness = 50f
-
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Parchment)
-    ) {
-        CircledRangeSlider(
-            onStartValueChange = {},
-            onFinishValueChange = {},
-            color = CircledRangeSliderDefaults.circledSliderColors(
-                inactiveTrackColor = Color.Red,
-                activeTrackColor = Color.Green
-            ),
-            paddingValues = PaddingValues(),
-            thumbRadius = thumbRadius,
-            activeTrackThickness = activeTrackThickness,
-            inactiveTrackThickness = inactiveTrackThickness,
-            modifier = Modifier
-                //.background(color = Color.Blue)
-                .fillMaxSize()
-        )
-    }
-}
 
 
 
